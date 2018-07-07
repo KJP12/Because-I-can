63ce710af6e39c41b0c34e0870f4909b0453b0dd
KJP12 <kjp12.k.joy.programer12@gmail.com>
2018-07-07 15:03:27 -0700

    Updates? Because I can.

--- whitespace/src/main/java/net/kjp12/bic/whitespace/Main.java
+++ whitespace/src/main/java/net/kjp12/bic/whitespace/Main.java
@@ -1,40 +1,51 @@
 package net.kjp12.bic.whitespace;//Created on 7/7/18.
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static java.lang.Integer.toHexString;
+import static java.lang.Integer.toUnsignedString;
+import static java.lang.System.out;
+
 public class Main {
+    private static final boolean[] b = new boolean[2];
+    private static int exit;
 
     /**
      * Iterates through every argument to iterate through every character to test if it's a whitespace.
-     * So even char ' ' (0x20) gets counted if allowed to be passed in.
-     * <p>
-     * Prints out the following if a whitespace is found
-     * <p>
-     * 1
-     * Found `u20` at 7 in `String Test`
-     * <p>
-     * then exits with 1, else it will print out '0' then exit with 0
-     * <p>
+     * If no arguments are available, it uses the Standard Input Stream as its source instead.
+     *
      * Why? Because I can.
      */
-    public static void main(String[] args) {
-        for (String s : args) {
-            char[] chars = s.toCharArray();
-            for (int i = 0; i < chars.length; i++) {
-                char c = chars[i];
-                if (Character.isWhitespace(c)) {
-                    System.out.println(1);
-                    System.out.print("Found `u");
-                    System.out.print(Integer.toHexString(c));
-                    System.out.print("` at ");
-                    System.out.print(i);
-                    System.out.print(" in `");
-                    System.out.print(s);
-                    System.out.println('`');
-                    System.exit(1);
-                    return;
-                }
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            try (InputStreamReader isr = new InputStreamReader(System.in)) {
+                char[] chars = new char[1024];
+                int index;
+                while ((index = isr.read(chars)) >= 0)
+                    testChars(chars, index);
+            }
+        } else for (String arg : args) {
+            out.println(arg);
+            testChars(arg.toCharArray());
+        }
+        out.println(exit);
+        System.exit(exit);
+    }
+
+    public static void testChars(char[] chars) {
+        testChars(chars, chars.length);
+    }
+
+    public static void testChars(char[] chars, int a) {
+        for (int i = 0; i < a; i++) {
+            if ((b[0] = Character.isWhitespace(chars[i])) || (b[1] = Character.isSpaceChar(chars[i]))) {
+                out.append("`u").append(toHexString(chars[i]))
+                        .append(b[0] && b[1] ? "` is white space & space char at " : b[0] ? "` is white space at " : "` is space char at ")
+                        .append(toUnsignedString(i))
+                        .println();
+                exit = 1;
             }
         }
-        System.out.println(0);
-        System.exit(0);
     }
 }
